d82ad1199bcc0dc84214dcecc1573976ab164209
Sean O'Beirne <sean.t.obeirne@gmail.com>
2024-03-11 16:39:47 -0400

    fixed blinking, began textbox

--- textwall.py
+++ textwall.py
@@ -40,21 +40,19 @@ stdout_buffer = "RUNNING LOG; POST EXECUTION OUTPUT:\n"
 DEBUG = True
 
 
-
-
-
 stdscr = curses.initscr()
+
 
 # Define colors
 curses.start_color()
 if curses.can_change_color():
     black_rgb = (0, 0, 0)
-    white_rgb = (1000, 1000, 1000)  # Adjust as needed for your terminal
+    white_rgb = (1000, 1000, 1000)
     red_rgb = (1000, 450, 450)
     green_rgb = (450, 1000, 450)
-    green2_rgb = (350, 800, 350)  # Custom green color 2
-    green3_rgb = (250, 600, 250)  # Custom green color 3
-    green4_rgb = (150, 400, 150)  # Custom green color 4
+    green2_rgb = (350, 800, 350)
+    green3_rgb = (250, 600, 250)
+    green4_rgb = (150, 400, 150)
 
     curses.init_color(curses.COLOR_BLACK, *black_rgb)
     curses.init_color(curses.COLOR_WHITE, *white_rgb)
@@ -78,30 +76,47 @@ GREEN4_AND_BLACK = curses.color_pair(6)
 
 x_pad = 1
 matrix_elements = []
+curses_lock = threading.Lock()
+mode = "command"
+
+
 
 class MatrixElement:
-    def __init__(self, stdscr, y, x, trail_length=5):
+    def __init__(self, stdscr, y, x, trail_length=8):
         self.stdscr = stdscr
         self.y = y
         self.x = x
         self.trail_length = trail_length
         self.trail = []
 
-
     def drop(self):
         self.y += 1
 
     def draw(self):
-        self.stdscr.addch(self.y, self.x, self.get_char(), curses.color_pair(3))
-        if self.y > 0:
-            self.stdscr.addch(self.y - 1, self.x, self.get_char(), curses.color_pair(4))
-        if self.y > 1:
-            self.stdscr.addch(self.y - 2, self.x, self.get_char(), curses.color_pair(5))
-        if self.y > 2:
-            self.stdscr.addch(self.y - 3, self.x, self.get_char(), curses.color_pair(6))
+        # TODO: make this smart
+        height, width = self.stdscr.getmaxyx()
+
+        green = curses.color_pair(3)
+        for line in range(self.trail_length):
+            draw_y = self.y - line
+            if 0 <= draw_y < height:
+                # Check if the new location is empty
+                if self.stdscr.inch(draw_y, self.x) == ord(' '):
+                    self.stdscr.addch(draw_y, self.x, self.get_char(), green)
+                    if line > 1:
+                        green = curses.color_pair(4)
+                    if line > 3:
+                        green = curses.color_pair(5)
+                    if line > 5:
+                        green = curses.color_pair(6)
+
+    def __str__(self):
+        return f"Matrix Element\n{self.y =}\n{self.x =}\n{self.trail_length =}\n{self.trail =}\n{stdscr.getmaxyx = }\n"
 
     def get_char(self):
         return randint(48, 90)
+
+
 
 ####################
 # HELPER FUNCTIONS #
@@ -109,6 +124,7 @@ class MatrixElement:
 
 # Exit gracefully, print from error buffer if debugging
 def clean_up(status):
+    stdscr.clear()
     curses.endwin()
     if DEBUG:
         print(stderr_buffer)
@@ -135,15 +151,15 @@ def get_x(width, string):
 
 
 # Draws beautiful boiletplate screen with info and menu
-def draw_frame(stdscr):
+def draw_frame():
 
     # Screen setup
     curses.curs_set(0)
     height, width = stdscr.getmaxyx()
-    stdscr.clear()
-    stdscr.refresh()
+    stdscr.erase()
+    # stdscr.refresh()
     
-    # draw_background(stdscr)
+    # draw_background()
 
     # Draw custom header and workspace border
     stdscr.attron(RED_AND_BLACK)
@@ -154,6 +170,7 @@ def draw_frame(stdscr):
         stdscr.addstr(start_y, start_x, heading)
         i += 1
     rectangle(stdscr, 4, x_pad, height - 1, width - 1 - x_pad)
+
     stdscr.attroff(RED_AND_BLACK)
 
 
@@ -185,34 +202,35 @@ def draw_frame(stdscr):
 
 def tick():
     while(True):
-        draw_frame(stdscr)
+        with curses_lock:
+            draw_frame()
         for ele in matrix_elements:
             ele.drop()
         
-        draw_elements(stdscr)
-        loto = randint(0,1)
-        if loto == 0:
-            spawn_element(stdscr)
-        time.sleep(0.05)
-        stdscr.refresh()
+        with curses_lock:
+            draw_elements()
+            loto = randint(0,1)
+            if loto == 0:
+                spawn_element()
+            time.sleep(0.05)
+            stdscr.refresh()
 
 
-def draw_elements(stdscr):
+def draw_elements():
     height, width = stdscr.getmaxyx()
     for matrix_element in matrix_elements:
-        if 0 < matrix_element.y < height and 0 < matrix_element.x < width:
-            try:
-                stdscr.attron(GREEN_AND_BLACK)
-                matrix_element.draw()
-                stdscr.attroff(GREEN_AND_BLACK)
-            except Exception as e:
-                add_to_stderr(str(e))
+        try:
+            matrix_element.draw()
+        except Exception as e:
+            add_to_stderr(str(e))
+            add_to_stderr(f"matrix element: {matrix_element}")
+            matrix_elements.remove(matrix_element)
     stdscr.refresh()
 
 
-def spawn_element(stdscr):
+def spawn_element():
     height, width = stdscr.getmaxyx()
-    combined_range = list(range(0, x_pad)) + list(range(width - x_pad, width))
+    combined_range = list(range(1, x_pad)) + list(range(width - x_pad, width - 1))
     random_number = random.choice(combined_range)
     matrix_element = MatrixElement(stdscr, 0, random_number)
     matrix_elements.append(matrix_element)
@@ -236,7 +254,35 @@ def get_input(stdscr, prompt):
 
     return filename
 
+def create_textbox():
+    height, width = stdscr.getmaxyx()
 
+    # Calculate textbox position and size
+    textbox_height = 3
+    textbox_width = width - 2 - x_pad
+    textbox_y = 5
+    textbox_x = x_pad
+
+    # Create a window for the textbox
+    textbox_win = curses.newwin(textbox_height, textbox_width, textbox_y, textbox_x)
+
+"""
+    # Draw a border around the textbox
+    textbox_win.border()
+
+    # Enable text input
+    curses.curs_set(1)
+    textbox_win.addstr(1, 1, "Enter text: ")
+
+    # Get user input
+    textbox_content = textbox_win.getstr(1, 12, textbox_width - 12).decode("utf-8")
+
+    # Disable text input
+    curses.curs_set(0)
+
+    return textbox_content
+"""
+"""
 def create_textbox():
     height, width = stdscr.getmaxyx()
     curses.curs_set(1)
@@ -245,7 +291,7 @@ def create_textbox():
     stdscr.refresh()
     box.edit()
     curses.curs_set(0)
-
+"""
 
 class MatrixThread(threading.Thread):
     def __init__(self):
@@ -270,10 +316,10 @@ def main(stdscr):
     else:
         x_pad = 1
 
-    stdscr.clear()
+    stdscr.erase()
 
 
-    draw_frame(stdscr)
+    draw_frame()
 
     try:
         background_thread = threading.Thread(target=tick)
@@ -286,24 +332,41 @@ def main(stdscr):
     # matrix_thread_instance = threading.Thread(target=matrix_thread, args=(stdscr,))
     # matrix_thread_instance.start()
     # matrix_thread.start()
+    global mode
 
     while True:
 
         sel = stdscr.getch()
 
-        if sel in(113, 81): # quit
-            clean_up(0)
-        elif sel in (43, 61):
-            x_pad -= 1
-        elif sel == 45:
-            x_pad += 1
-        elif sel == 32:
-            pass
-        elif sel in (73, 105):
-            create_textbox()
+        if mode == "command":
+            if sel in(113, 81): # quit, q/Q
+                clean_up(0)
+            elif sel in (43, 61): # grow box, +/=
+                x_pad -= 1
+            elif sel == 45: # shrink box, -
+                x_pad += 1
+            elif sel == 32: # pass space
+                pass
+            elif sel in (73, 105): # insert mode, i/I
+                mode = "insert"
+                create_textbox()
+        elif mode == "insert":
+            if sel in(113, 81): # quit
+                clean_up(0)
+            elif sel in (43, 61):
+                x_pad -= 1
+            elif sel == 45:
+                x_pad += 1
+            elif sel == 32:
+                pass
+            elif sel in (73, 105):
+                mode = "insert"
+                create_textbox()
+                textbox_content = create_textbox()
+                # create_textbox(stdscr, height // 2, width // 2, 20)
 
 
-
+        stdscr.erase()
         stdscr.refresh()
 
 
